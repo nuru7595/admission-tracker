e2e86daf0d573673a466a53833444248e122ba6a
Nuru <nuruzzamannuru7595@gmail.com>
2025-02-23 15:03:10 +0600

    Weekly Fixed

--- admission-tracker/src/App.jsx
+++ admission-tracker/src/App.jsx
@@ -30,56 +30,56 @@ export default function App() {
                                     <td>Bangla 1st</td>
                                     <td>80 Pages</td>
                                     <td>{`${Math.ceil(80 / 2)} Pages`}</td>
-                                    <td>{`${Math.ceil(80 / 4)} Pages`}</td>
+                                    <td>{`${Math.ceil(80 / 8)} Pages`}</td>
                                     <td>{`${Math.ceil(80 / 67)} Pages`}</td>
                                 </tr>
                                 <tr>
                                     <td>Bangla 2nd</td>
                                     <td>76 Pages</td>
                                     <td>{`${Math.ceil(76 / 2)} Pages`}</td>
-                                    <td>{`${Math.ceil(76 / 4)} Pages`}</td>
+                                    <td>{`${Math.ceil(76 / 8)} Pages`}</td>
                                     <td>{`${Math.ceil(76 / 67)} Pages`}</td>
                                 </tr>
                                 <tr>
                                     <td>English</td>
                                     <td>90 Pages</td>
                                     <td>{`${Math.ceil(90 / 2)} Pages`}</td>
-                                    <td>{`${Math.ceil(90 / 4)} Pages`}</td>
+                                    <td>{`${Math.ceil(90 / 8)} Pages`}</td>
                                     <td>{`${Math.ceil(90 / 67)} Pages`}</td>
                                 </tr>
                                 <tr>
                                     <td>Civics</td>
                                     <td>18 Pages</td>
                                     <td>{`${Math.ceil(18 / 2)} Pages`}</td>
-                                    <td>{`${Math.ceil(18 / 4)} Pages`}</td>
+                                    <td>{`${Math.ceil(18 / 8)} Pages`}</td>
                                     <td>{`${Math.ceil(18 / 67)} Page`}</td>
                                 </tr>
                                 <tr>
                                     <td>Economics</td>
                                     <td>20 Pages</td>
                                     <td>{`${Math.ceil(20 / 2)} Pages`}</td>
-                                    <td>{`${Math.ceil(20 / 4)} Pages`}</td>
+                                    <td>{`${Math.ceil(20 / 8)} Pages`}</td>
                                     <td>{`${Math.ceil(20 / 67)} Page`}</td>
                                 </tr>
                                 <tr>
                                     <td>Sociology</td>
                                     <td>14 Pages</td>
                                     <td>{`${Math.ceil(14 / 2)} Pages`}</td>
-                                    <td>{`${Math.ceil(14 / 4)} Pages`}</td>
+                                    <td>{`${Math.ceil(14 / 8)} Pages`}</td>
                                     <td>{`${Math.ceil(14 / 67)} Page`}</td>
                                 </tr>
                                 <tr>
                                     <td>Psychology</td>
                                     <td>14 Pages</td>
                                     <td>{`${Math.ceil(14 / 2)} Pages`}</td>
-                                    <td>{`${Math.ceil(14 / 4)} Pages`}</td>
+                                    <td>{`${Math.ceil(14 / 8)} Pages`}</td>
                                     <td>{`${Math.ceil(14 / 67)} Page`}</td>
                                 </tr>
                                 <tr>
                                     <td>GK</td>
                                     <td>50 Pages</td>
                                     <td>{`${Math.ceil(50 / 2)} Pages`}</td>
-                                    <td>{`${Math.ceil(50 / 4)} Pages`}</td>
+                                    <td>{`${Math.ceil(50 / 8)} Pages`}</td>
                                     <td>{`${Math.ceil(50 / 67)} Page`}</td>
                                 </tr>
                             </tbody>
