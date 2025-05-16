f9ca2b79912b5b551ce879b9b4322657d8c9852d
Nuru <nuruzzamannuru7595@gmail.com>
2025-05-16 09:02:21 +0600

    admit added

--- admission-tracker/src/components/Preparation.jsx
+++ admission-tracker/src/components/Preparation.jsx
@@ -4,54 +4,26 @@ export default function Preparation() {
             <h3 className="section-title">Exam Essentials</h3>
             <div className="section-container">
                 <p>
-                    1.{" "}
                     <span className="font-bold text-green-600">Admit Card</span>{" "}
                     =&gt;
                     <a
-                        className="font-bold text-sm bg-blue-600 px-3 py-[2px] rounded-md mx-2"
+                        className="font-bold text-sm bg-green-600 px-3 py-[2px] rounded-md mx-2"
                         href="http://app55.nu.edu.bd/nu-web/applicantLogin.action?degreeName=Honours"
                         target="_blank"
                     >
-                        Download Now
+                        Download Here
                     </a>
                 </p>
                 <p>
-                    2. HSC{" "}
+                    HSC{" "}
                     <span className="font-bold text-green-600">
                         Registration Card
                     </span>
                 </p>
                 <p>
-                    3. Up to 3-4 Black{" "}
+                    Up to 3-4 Black{" "}
                     <span className="font-bold text-green-600">
                         Ballpoint Pens
-                    </span>
-                </p>
-                <p>
-                    4. A Scientific{" "}
-                    <span className="font-bold text-green-600">Calculator</span>{" "}
-                    (Optional)
-                </p>
-            </div>
-
-            <h3 className="section-title">Times to Notice</h3>
-            <div className="section-container">
-                <p>
-                    1. Start Getting Ready:{" "}
-                    <span className="font-bold text-green-600">8:00 AM</span>
-                </p>
-                <p>
-                    2. Leave for College:{" "}
-                    <span className="font-bold text-green-600">9:00 AM</span>
-                </p>
-                <p>
-                    3. Be Seated in Exam Hall By:{" "}
-                    <span className="font-bold text-green-600">10:30 AM</span>
-                </p>
-                <p>
-                    4. Exam Time:{" "}
-                    <span className="font-bold text-green-600">
-                        11:00 AM – 12:00 PM
                     </span>
                 </p>
             </div>
